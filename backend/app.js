1ce4e8de7415adfd24eff05b855b751bdf6e2abf
anujgupta5686 <anujgupta5686@gmail.com>
2025-01-25 17:20:42 +0530

    Cors Update

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://todo-nine-olive-52.vercel.app/",
+    origin: "https://todo-nine-olive-52.vercel.app",
     credentials: true,
   })
 );
